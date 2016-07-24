8a587cc3d1890219b728ce9fd5922867c7699d79
Daniel Loeffelholz <dloeffel@thoughtworks.com>
2016-07-24 13:58:42 +0200

    dl||package structure

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,20 +2,38 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var gulprm = require('gulp-rm');
+var runSequence = require('run-sequence');
 
-gulp.task('build', function () {
+gulp.task('buildJS', ['copyStaticResources'], function () {
   return browserify({entries: './src/js/app.js', extensions: ['.js'], debug: true})
     .transform(babelify)
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('watch', ['build'], function () {
-  gulp.watch('*.js', ['build']);
+  gulp.watch(['*.js', '*.html', '*.css'], ['build']);
 });
 
-gulp.task('default', ['build']);
+gulp.task('copyStaticResources', function () {
+  gulp.src('./src/css/**/*')
+    .pipe(gulp.dest('dist/css'));
+
+  gulp.src('./src/js/**/*')
+    .pipe(gulp.dest('dist/js'));
+
+  gulp.src('./src/index.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src(['./dist/**']).pipe(gulprm());
+});
+
+
+gulp.task('default', (cb) => runSequence(['clean'], ['copyStaticResources', 'buildJS'], cb));
 
 
 // TODO: copy static resources (css, html) into dist when building
